00deb3f9c0f18c42af96cb4b90fc6cdfd7b3209a
Santiago Álvarez <A01640172@itesm.mx>
2024-11-25 20:33:51 -0600

    implemented file copying into destination

--- Cabinet.py
+++ Cabinet.py
@@ -1,8 +1,11 @@
 from exif import Image
 from geopy.geocoders import Nominatim
 import os
+import shutil
+from unidecode import unidecode
 
-base_dir = 'TestFiles'
+image_dir = 'TestFiles'
+destination_dir = 'SortedFiles'
 
 geoloc = Nominatim(user_agent="GetLoc")
 
@@ -19,26 +22,22 @@ def image_information(image_path):
     with open(image_path, 'rb') as src:
         img = Image(src)
 
-    if img.has_exif:
+    if img.has_exif and img.datetime_original is not None:
         try:
             coords = (decimal_coords(img.gps_latitude,
                       img.gps_latitude_ref),
                       decimal_coords(img.gps_longitude,
                       img.gps_longitude_ref))
         except AttributeError:
-            print ('No Coordinates Available')
+            return({"coordinates":False, "year":img.datetime_original.split(":")[0]})
     else:
-        print('The Image has no EXIF information')
+        raise Exception('The Image has no EXIF information')
         
-    return({"year":img.datetime_original.split(":")[0], "latitude":coords[0],"longitude":coords[1]})
+    return({"coordinates":True, "year":img.datetime_original.split(":")[0], "latitude":coords[0],"longitude":coords[1]})
 
-for file in os.listdir(base_dir):
-    if file.endswith(".jpg"):
-
-        #Analyze an image
-        info = image_information(base_dir + "/" + file)
+def make_path(info):
+    if info["coordinates"]:
         location = geoloc.reverse(str(info["latitude"]) + "," + str(info["longitude"]), language="en").raw['address']
-        
 
         country = location['country']
 
@@ -60,6 +59,33 @@ for file in os.listdir(base_dir):
         #print(location)
 
         if county is not None:
-            print(file + ": " + county + ", " + country + " (" + info["year"] + ")")
+            #print(file + ": " + county + ", " + country + " (" + info["year"] + ")")
+            path = "/".join([destination_dir, info["year"], country, county])
         else:
-            print(file + ": " + town + ", " + country + " (" + info["year"] + ")")
+            #print(file + ": " + town + ", " + country + " (" + info["year"] + ")")
+            path = "/".join([destination_dir, info["year"], country, town])
+        
+        path = unidecode(path)
+    else:
+        path = "/".join([destination_dir, info["year"], "Unknown"])
+
+    return path
+
+for file in os.listdir(image_dir):
+    if file.endswith(".jpg"):
+
+        #Analyze an image
+        try:
+            info = image_information(image_dir + "/" + file)
+
+            path = make_path(info)
+
+        except:
+            print(f"Error. Image {file} couldn't be classified. Moving to {destination_dir}/Unknown/")
+            os.makedirs(os.path.dirname(f"{destination_dir}/Unknown/"), exist_ok=True)
+            shutil.copyfile(f"{image_dir}/{file}", f"{destination_dir}/Unknown/{file}")
+        
+        else:
+            print(path)
+            os.makedirs(os.path.dirname(path + '/'), exist_ok=True)
+            shutil.copyfile(f"{image_dir}/{file}", f"{path}/{file}")
